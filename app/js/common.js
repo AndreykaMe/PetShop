1e251cd51da4d85a71c3f8145d5aa1db5adec15e
Andrey <andystory@mail.ru>
2017-09-07 02:27:10 +0700

    Create header, no-mobile, with JS-events.

--- app/js/common.js
+++ app/js/common.js
@@ -8,4 +8,22 @@ $(function() {
         }
     });
 
+	$(".header-hmb").on('click', function() {
+        $(".header-cat").css('transform', 'translateX(0%)');
+        $(".header-dog").css('transform', 'translateX(0%)');
+        $(".header-hmb").fadeOut('slow');
+    });
+
+	$(".header-dog").on('click', function() {
+	    $(".header-cat").css('transform', 'translateX(100%)');
+        $(".header-dog").css('transform', 'translateX(50%)');
+        $(".header-hmb").fadeIn('slow');
+    });
+
+    $(".header-cat").on('click', function() {
+        $(".header-cat").css('transform', 'translateX(-50%)');
+        $(".header-dog").css('transform', 'translateX(-100%)');
+        $(".header-hmb").fadeIn('slow');
+    });
+
 });
