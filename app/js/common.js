254b1fe2fa91cc5df5c52d248729a7a4d3c4e7d4
Andrey <andystory@mail.ru>
2017-09-07 02:57:51 +0700

    Create header-mobile with all screen-resolution

--- app/js/common.js
+++ app/js/common.js
@@ -2,28 +2,44 @@ $(function() {
 
 	$(".navbar-toggle").on('click', function() {
 	    if($(".navbar-collapse").hasClass('in')) {
-            $(".hamburger").removeClass('is-active');
+            $(".navbar-toggle").removeClass('is-active');
         } else {
-            $(".hamburger").addClass('is-active');
+            $(".navbar-toggle").addClass('is-active');
         }
     });
 
+
 	$(".header-hmb").on('click', function() {
-        $(".header-cat").css('transform', 'translateX(0%)');
-        $(".header-dog").css('transform', 'translateX(0%)');
+        if(screen.width > 480) {
+            $(".header-cat").css('transform', 'translateX(0%)');
+            $(".header-dog").css('transform', 'translateX(0%)');
+        } else {
+            $(".header-cat").fadeIn('slow');
+            $(".header-dog").fadeIn('slow');
+        }
         $(".header-hmb").fadeOut('slow');
     });
 
 	$(".header-dog").on('click', function() {
-	    $(".header-cat").css('transform', 'translateX(150%)');
-        $(".header-dog").css('transform', 'translateX(50%)');
-        $(".header-hmb").fadeIn('slow');
+	    if(screen.width > 480) {
+            $(".header-cat").css('transform', 'translateX(150%)');
+            $(".header-dog").css('transform', 'translateX(50%)');
+            $(".header-hmb").fadeIn('slow');
+        } else {
+            $(".header-cat").fadeOut('slow');
+            $(".header-hmb").fadeIn('slow');
+        }
     });
 
     $(".header-cat").on('click', function() {
-        $(".header-cat").css('transform', 'translateX(-50%)');
-        $(".header-dog").css('transform', 'translateX(-150%)');
-        $(".header-hmb").fadeIn('slow');
+        if(screen.width > 480) {
+            $(".header-cat").css('transform', 'translateX(-50%)');
+            $(".header-dog").css('transform', 'translateX(-150%)');
+            $(".header-hmb").fadeIn('slow');
+        } else {
+            $(".header-dog").fadeOut('slow');
+            $(".header-hmb").fadeIn('slow');
+        }
     });
 
 });
